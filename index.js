47ad60bc3f0386e7ef1ca9c1319bbd91bb7fd427
Mehebul02 <mehebul2123@gmail.com>
2024-06-02 00:28:15 +0600

    single data get in data base

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -8,7 +8,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    // 'https://pet-adoption-ed896.web.app'
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -57,20 +61,31 @@ async function run() {
         next();
       });
     };
-    // user relate api 
-   app.post('/users',async(req,res)=>{
-    const user = req.body
-    const result =await usersCollection.insertOne(user)
-    res.send(result)
-   })
+    // user relate api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+      const exitingUser = await usersCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "user already exit", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
     // pets relate api
     app.get("/pets", async (req, res) => {
       const category = req.query.category;
       let query = {};
-      if (category && category !== 'null') {
+      if (category && category !== "null") {
         query = { category };
       }
       const result = await petsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/pet/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petsCollection.findOne(query);
       res.send(result);
     });
     console.log(

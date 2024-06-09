5dc91cc97ec577723a3bda58a92553d872c5a329
Mehebul02 <mehebul2123@gmail.com>
2024-06-10 02:23:59 +0600

    admin problem solve

--- index.js
+++ index.js
@@ -55,36 +55,101 @@ async function run() {
       res.send({ token });
     });
 
-    // middlewares
+    // middleWere
     const verifyToken = (req, res, next) => {
-      // console.log('inside verify token', req.headers.authorization);
+      console.log(req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized access" });
+        return res.status(401).send({ message: "Unauthorized access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "unauthorized access" });
+          return res.status(401).send({ message: "Unauthorized access" });
         }
         req.decoded = decoded;
         next();
       });
+      // next()
     };
+      // use verify admin after verifyToken
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === 'admin';
+        if (!isAdmin) {
+          return res.status(403).send({ message: 'forbidden access' });
+        }
+        next();
+      }
     // user relate api
+    //  app.get("/allUsers", verifyToken,verifyAdmin, async (req, res) => {
+    //   const result = await usersCollection.find().toArray();
+    //   res.send(result);
+    // });
+    app.get("/allUsers",verifyToken,verifyAdmin,  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.patch("/users/admin/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user?.email };
+      const query = { email: user.email };
       const exitingUser = await usersCollection.findOne(query);
       if (exitingUser) {
-        return res.send({ message: "user already exit", insertedId: null });
+        return res.send({ message: "user already exits", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
     // pets relate api
     app.post("/pets", async (req, res) => {
       const pets = req.body;
       const result = await petsCollection.insertOne(pets);
+      res.send(result);
+    });
+    // single pet
+    app.get("/all-pets", async (req, res) => {
+      const result = await petsCollection.find().toArray();
+      res.send(result);
+    });
+    app.patch("/pets-status/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          ...status,
+        },
+      };
+      const result = await petsCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
     app.get("/pets", async (req, res) => {
@@ -144,34 +209,32 @@ async function run() {
       res.send(result);
     });
 
-
-
     // Adopt relate api
     app.post("/adopts", async (req, res) => {
       const adopt = req.body;
       const result = await adoptCollection.insertOne(adopt);
       res.send(result);
     });
-    
+
     app.get("/my-request-adopts/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const result = await adoptCollection.find(query).toArray()
+      const result = await adoptCollection.find(query).toArray();
       res.send(result);
     });
-    // status update 
-    app.patch('/adopt/:id',async(req,res)=>{
-      const id = req.params.id 
-      const status  = req.body
-      const query ={_id : new ObjectId(id)}
-      const updatedDoc={
-        $set:{
-          ...status
-        }
-      }
-      const result = await adoptCollection.updateOne(query,updatedDoc)
-      res.send(result)
-    })
+    // status update
+    app.patch("/adopt/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          ...status,
+        },
+      };
+      const result = await adoptCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
     // donation relate api
 
     app.get("/donation", async (req, res) => {
